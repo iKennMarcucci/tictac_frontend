1ed4b080088c0a7d292e0f5670d52446ea4c17bd
iKennMarcucci <marcucci5783@gmail.com>
2023-06-16 13:42:06 -0500

    Validaciones Hechas

--- src/features/directivos/pages/Docentes.tsx
+++ src/features/directivos/pages/Docentes.tsx
@@ -2,7 +2,8 @@ import { Header } from "@/components";
 import { TableDocentes } from "@/features/directivos/components";
 import { FiSearch } from "react-icons/fi";
 import ReactModal from "react-modal";
-import {useState} from "react";
+import { useState } from "react";
+import Alert from '@mui/material/Alert';
 import axios from "@/utils/AxiosInstance.tsx";
 
 const Docentes = () => {
@@ -11,6 +12,8 @@ const Docentes = () => {
 
   const [idLinea, setIdLinea] = useState("0");
   const [esLider, setEsLider] = useState("0");
+
+  const [xd, setXd] = useState(0);
 
   const handleIdLineaChange = (event) => {
     setIdLinea(event.target.value);
@@ -50,8 +53,10 @@ const Docentes = () => {
         });
 
         console.log('Archivo enviado:', response.data);
+        setXd(1);
       } catch (error) {
         console.error('Error al enviar el archivo:', error);
+        setXd(2);
       }
     }
     handleCloseModal();
@@ -60,31 +65,22 @@ const Docentes = () => {
   return (
     <div className="mt-3">
       <Header titulo="Docentes" subtitulo="Listado" />
+      {xd === 1 ? (
+        <Alert severity="error" onClose={() => { }}><strong>ERROR</strong> al cargar los datos.</Alert>
+      ) : xd === 2 ? (
+        <Alert onClose={() => { }}>Datos Cargados en el Sistema.</Alert>
+      ) : (
+        <p></p>
+      )}
       <div className="bg-white mt-4 shadow-md p-3">
         <div className="flex justify-between items-center">
           <div className="flex gap-10">
-            {/* Barra busqueda nombre */}
-            {/*
-            <div>
-              <p className="font-medium">Nombre docente</p>
-              <div className="flex items-center gap-2 focus:outline-none rounded-lg border border-gray-300 hover:bg-gray-100 bg-white mt-2">
-                <div className="p-2 rounded-tl-lg rounded-bl-lg">
-                  <FiSearch className="" size={20} />
-                </div>
-                <input
-                  className="focus:outline-none border-none bg-transparent py-2 px-2"
-                  type="text"
-                  placeholder="Buscar..."
-                />
-              </div>
-            </div>
-            */}
             {/* Select filter by idLinea */}
             <div>
               <p className="font-medium">Linea Transversal</p>
               <select className="appearance-none bg-white border border-gray-300 py-2 px-4 pr-8 rounded-lg cursor-pointer hover:bg-gray-100 focus:outline-none focus:shadow-outline mt-2"
-                      value={idLinea}
-                      onChange={handleIdLineaChange}
+                value={idLinea}
+                onChange={handleIdLineaChange}
               >
                 <option value="0">Seleccione una Linea Transversal</option>
                 <option value="1">1. Relaciones sociales y prácticas cívicas </option>
@@ -99,8 +95,8 @@ const Docentes = () => {
             <div>
               <p className="font-medium">Tipo de docente</p>
               <select className="appearance-none bg-white border border-gray-300 py-2 px-4 pr-8 rounded-lg cursor-pointer hover:bg-gray-100 focus:outline-none focus:shadow-outline mt-2"
-                      value={esLider}
-                      onChange={handleEsLiderChange}
+                value={esLider}
+                onChange={handleEsLiderChange}
               >
                 <option className="" value="0">Seleccione un tipo de docente</option>
                 <option className="" value="2">Lider PPT</option>
@@ -114,31 +110,31 @@ const Docentes = () => {
             Cargar docentes
           </button>
           <ReactModal
-              isOpen={modalVisible}
-              onRequestClose={handleCloseModal}
-              // Otras propiedades del modal
-              style={{
-                overlay: {
-                  backgroundColor: 'rgba(255, 255, 255, 0.5)',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                },
-                content: {
-                  top: '50%',
-                  left: '50%',
-                  transform: 'translate(-50%, -50%)',
-                  width: '450px',
-                  height: '200px',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                },
-              }}
+            isOpen={modalVisible}
+            onRequestClose={handleCloseModal}
+            // Otras propiedades del modal
+            style={{
+              overlay: {
+                backgroundColor: 'rgba(255, 255, 255, 0.5)',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              },
+              content: {
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                width: '450px',
+                height: '200px',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+              },
+            }}
           >
             {/* Contenido del modal */}
-            <h1 style={{fontSize: '24px', marginBottom: '16px'}}>Cargar docentes</h1>
+            <h1 style={{ fontSize: '24px', marginBottom: '16px' }}>Cargar docentes</h1>
             <form onSubmit={handleSubmit} style={{
               display: 'flex',
               flexDirection: 'column',
@@ -147,7 +143,7 @@ const Docentes = () => {
             }}>
               <label>
                 <input name={"file"} id={"file"} type="file" onChange={handleInputChange}
-                       className="bg-gray-100 border border-gray-300 rounded-lg p-1 mt-1"/>
+                  className="bg-gray-100 border border-gray-300 rounded-lg p-1 mt-1" />
               </label>
               <div style={{
                 width: '50%',
@@ -156,16 +152,16 @@ const Docentes = () => {
                 marginTop: '16px'
               }}>
                 <button type="submit"
-                        className="bg-green-500 hover:bg-green-300 text-white font-medium rounded-lg h-max p-2">Enviar
+                  className="bg-green-500 hover:bg-green-300 text-white font-medium rounded-lg h-max p-2">Enviar
                 </button>
                 <button onClick={handleCloseModal}
-                        className="bg-red-500 hover:bg-red-300 text-white font-medium rounded-lg h-max p-2">Cancelar
+                  className="bg-red-500 hover:bg-red-300 text-white font-medium rounded-lg h-max p-2">Cancelar
                 </button>
               </div>
             </form>
           </ReactModal>
         </div>
-        <TableDocentes idLinea={idLinea} esLider={esLider}/>
+        <TableDocentes idLinea={idLinea} esLider={esLider} />
       </div>
     </div>
   );
