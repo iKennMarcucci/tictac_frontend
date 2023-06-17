2167c3af6bd59681cb41089ae07b3b57482c59cc
Diego-Pedrozo <diegopedrozovillarreal@gmail.com>
2023-06-17 13:09:53 -0500

    Ajustes de despliegue

--- src/utils/AxiosInstance.tsx
+++ src/utils/AxiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8081', // Ruta base de tu API
+    baseURL: 'https://tictacbackend-production.up.railway.app', // Ruta base de tu API
     headers: {
         // Opciones de encabezado comunes, si es necesario
         'Content-Type': 'application/json',
